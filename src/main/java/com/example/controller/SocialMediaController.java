a4ca6ebc25aed182752a6bce236203d232b3bc10
Duc Le <ducle147@gmail.com>
2024-12-19 17:21:18 +0000

    Log in Account

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -17,19 +17,30 @@ import com.example.exception.UserAlreadyExistsException;
 @RestController
 public class SocialMediaController {
     
-    @Autowired
     private AccountService accountService;
 
-    // @Autowired
+    @Autowired
     public SocialMediaController(AccountService accountService) {
         this.accountService = accountService;
     }
     
     @PostMapping("/register")
     public ResponseEntity<Account> register(@RequestBody Account account) {
-        Account toRet = accountService.registerUser(account);
-        return ResponseEntity.status(HttpStatus.OK).body(toRet);
+        Account regAcc = accountService.registerUser(account);
+        return ResponseEntity.status(HttpStatus.OK).body(regAcc);
+    }
 
+    // @PostMapping(path = "/login")
+    // public @ResponseBody ResponseEntity<Account> login(@RequestBody Account account)
+    // {
+    //     Account loggedInAcc = accountService.login(account);
+    //     return ResponseEntity.status(HttpStatus.OK).body(loggedInAcc);
+    // }
+
+    @PostMapping(path = "/login")
+    public @ResponseBody ResponseEntity<Account> login(@RequestBody Account account) throws AuthenticationException
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(accountService.login(account));
     }
 
     @ExceptionHandler(InvalidRegistrationException.class)
@@ -45,5 +56,11 @@ public class SocialMediaController {
     {
         return e.getMessage();
     }
-}
 
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public @ResponseBody String handleInvalidAuthentication(AuthenticationException e)
+    {
+        return e.getMessage();
+    }
+}
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -4,6 +4,7 @@ import com.example.entity.Account;
 import com.example.repository.AccountRepository;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.entity.Account;
 import com.example.repository.AccountRepository;
@@ -24,19 +25,20 @@ public class AccountService {
     {
         String username = account.getUsername();
         String password = account.getPassword();
-        if(username == "" || password.length() < 4)
-        {
+        if(username == "" || password.length() < 4) {
             throw new InvalidRegistrationException();
-        }
-        else if(accountRepository.findByUsername(username).isPresent())
-        {
+        } else if(accountRepository.findByUsername(username).isPresent()) {
             throw new UserAlreadyExistsException();
-        }
-        else
-        {
+        } else {
             return accountRepository.save(account);
         }
     }
 
+    public Account login(Account account) throws AuthenticationException
+    {
+        return accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword())
+            .orElseThrow(() -> new AuthenticationException(null));
+    }
+    
     
 }
